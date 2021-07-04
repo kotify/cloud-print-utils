b61c0ffc4633c3bbf91c1e8762742d683dd2f2d3
Konstantin Alekseev <mail@kalekseev.com>
2021-07-04 12:02:50 +0300

    Bump wkhtmltopdf

--- wkhtmltox/layer_builder.sh
+++ wkhtmltox/layer_builder.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
-# wkhtmltox version, on change verify rpm download url
-# it can be different from "$VERSION-1"
-export VERSION=0.12.5
+export VERSION="0.12.6-1"
 
 cd /tmp/
 yum install -y yum-utils rpmdevtools
@@ -21,13 +19,7 @@ rpmdev-extract -- *rpm
 mkdir /opt/lib
 cp -P -r /tmp/*/usr/lib64/* /opt/lib/
 
-export DOWNLOAD_URL="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/$VERSION"
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 8C64CF2A
-curl -LO "$DOWNLOAD_URL/SHA256SUMS"
-curl -LO "$DOWNLOAD_URL/SHA256SUMS.asc"
-gpg --verify SHA256SUMS.asc SHA256SUMS
-curl -LO "$DOWNLOAD_URL/wkhtmltox-$VERSION-1.centos7.x86_64.rpm"
-grep centos7.x86_64 SHA256SUMS | sha256sum --check
+curl -LO "https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/$VERSION/wkhtmltox-$VERSION.centos7.x86_64.rpm"
 rpmdev-extract wkhtmltox-*rpm
 mkdir /opt/bin
 cp /tmp/*/usr/local/bin/wkhtmlto* /opt/bin/
