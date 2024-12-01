6308d313a743abc4808f9a8fb21eab8b741ce235
Konstantin Alekseev <konstantin@kotify.com>
2024-12-01 21:21:23 +0200

    remove deprecated wkhtmltox

--- cdk-stacks/bin/app.ts
+++ cdk-stacks/bin/app.ts
@@ -2,8 +2,6 @@
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { WeasyPrintStack } from "../lib/weasyprint-stack";
-import { WkhtmltoxStack } from "../lib/wkhtmltox-stack";
 
 const app = new cdk.App();
 new WeasyPrintStack(app, "WeasyPrintStack");
-new WkhtmltoxStack(app, "WkhtmltoxStack");
